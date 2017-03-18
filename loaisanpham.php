b97e312d2a3542eba7e20d45ce9e7b826c8b9cf1
Le Trung Hung <supperstarlk@gmail.com>
2017-03-18 12:07:10 +0700

    dangnhap dang ky

--- loaisanpham.php
+++ loaisanpham.php
@@ -1,27 +1,94 @@
+
 <?php
-include_once("config.php");
-
-$maloaicha = $_POST["maloaicha"];
-$truyvan = "SELECT *  FROM loaisanpham WHERE MALOAI_CHA = " .$maloaicha ;
-$ketqua = mysqli_query($conn, $truyvan);
-$chuoijson = array();
-echo "{";
-echo "\"LOAISANPHAM\":";
-if($ketqua){
-    while ($dong=mysqli_fetch_array($ketqua)) {
-        // cách 1
-       // $chuoijson[] = $dong;
-
-        // end cách 1
-        // laydanhsachloaisp($dong["MALOAISP"]);
-
-        //cách 2
-         array_push($chuoijson, array("TENLOAISP"=>$dong["TENLOAISP"],'MALOAISP' => $dong["MALOAISP"]));
-        //end cách 2
+    include_once("config.php");
+    $ham = $_POST["ham"];
+    switch($ham){
+        case 'LayDanhSachMenu':
+            $ham(); // Lay danh sach menu
+            break;
+        case 'DangKyThanhVien':
+            $ham();
+            break;
+        case 'KiemTraDangNhap':
+            $ham();
+            break;
     }
 
-    echo json_encode($chuoijson,JSON_UNESCAPED_UNICODE);
-}
-echo "}";
+    function KiemTraDangNhap(){
+        global $conn;
+        if(isset($_POST["tendangnhap"]) || isset($_POST["matkhau"])){
+            $tendangnhap = $_POST["tendangnhap"];
+            $matkhau = $_POST["matkhau"];
+        }
 
+        $truyvan = "SELECT * FROM nhanvien WHERE TENDANGNHAP='".$tendangnhap."' AND MATKHAU='".$matkhau."'";
+        $ketqua = mysqli_query($conn,$truyvan);
+        $demdong = mysqli_num_rows($ketqua);
+        if($demdong >=1){
+            $tennv = "";
+            while ($dong = mysqli_fetch_array($ketqua)) {
+                $tennv = $dong["TENNV"];
+            }
+            echo "{ ketqua : true, tennv : \"".$tennv."\" }";
+        }else{
+            echo "{ ketqua : false }";
+        }
+
+    }
+    function DangKyThanhVien(){
+        global $conn;
+        if(isset($_POST["tennv"]) || isset($_POST["tendangnhap"]) || isset($_POST["matkhau"]) || isset($_POST["maloainv"]) || isset($_POST["emaildocquyen"])){
+            $tennv = $_POST["tennv"];
+            $tendangnhap = $_POST["tendangnhap"];
+            $matkhau = $_POST["matkhau"];
+            $maloainv = $_POST["maloainv"];
+            $emaildocquyen = $_POST["emaildocquyen"];
+        }
+
+        $truyvan = "SELECT * FROM nhanvien WHERE TENDANGNHAP='".$tendangnhap."' ";
+        $checkdk = mysqli_query($conn,$truyvan);
+        $demdong = mysqli_num_rows($checkdk);
+        if($demdong >=1){
+            echo "{ ketqua : false }";
+        }else{
+            $truyvan = "INSERT INTO nhanvien (TENNV, TENDANGNHAP,MATKHAU,MALOAINV,EMAILDOCQUYEN) VALUES ('".$tennv."', '".$tendangnhap."', '".$matkhau."','".$maloainv."','".$emaildocquyen."') " ;
+            if(mysqli_query($conn,$truyvan)){
+                echo "{ ketqua : true, tennv : \"".$tennv."\" }";
+            }else{
+                echo "{ ketqua : false }".mysqli_error($conn);
+            }
+        }
+
+        mysqli_close($conn);
+    }
+
+    function LayDanhSachMenu(){
+        global $conn;
+        if(isset($_POST["maloaicha"]))
+            $maloaicha = $_POST["maloaicha"];
+
+        $truyvancha = "SELECT *  FROM loaisanpham WHERE MALOAI_CHA = ".$maloaicha;
+        $ketqua = mysqli_query($conn,$truyvancha);
+        $chuoijson = array();
+        echo "{";
+        echo "\"LOAISANPHAM\":";
+        if($ketqua){
+            while ($dong=mysqli_fetch_array($ketqua)) {
+                // cách 1
+                $chuoijson[] = $dong;
+
+                // end cách 1
+                // laydanhsachloaisp($dong["MALOAISP"]);
+
+                //cách 2
+                // array_push($chuoijson, array("TENLOAISP"=>$dong["TENLOAISP"],'MALOAISP' => $dong["MALOAISP"]));
+                //end cách 2
+            }
+
+            echo json_encode($chuoijson,JSON_UNESCAPED_UNICODE);
+        }
+        echo "}";
+
+        mysqli_close($conn);
+    }
 ?>
